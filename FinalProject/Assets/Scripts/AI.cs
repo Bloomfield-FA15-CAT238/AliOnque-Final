155f5c159af7a883b97540cd77f704ecbc0f8153
Ali Onque <ali_onque@bloomfield.edu>
2015-12-16 16:19:30 -0500

    AI N WATER
    added a basic ai (1 hit KO if they touch you) and put some water on the
    floor (You can't swim, try jumping off lvl 2)

---
+++ FinalProject/Assets/Scripts/AI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AI : MonoBehaviour {
+
+	private GameObject target;
+	private NavMeshAgent agent;
+	private Transform dest;
+
+	// Use this for initialization
+	void Start () {
+		target = GameObject.FindGameObjectWithTag ("Player");
+		agent = GetComponent<NavMeshAgent> ();
+		dest = target.transform;
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		agent.SetDestination (dest.position);
+
+	}
+}
